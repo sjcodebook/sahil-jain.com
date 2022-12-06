3b0b4436ba52a70dd12f53f240e87d2f46b6c81d
0xSJ <jainsanmati846@gmail.com>
2022-12-06 14:48:13 +0530

    remove unwanted files

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'i.scdn.co', // Spotify Album Art
-      'pbs.twimg.com', // Twitter Profile Picture
-      'cdn.sanity.io'
-    ]
+    domains: []
   },
   experimental: {
     fontLoaders: [
