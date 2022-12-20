9d6e07e7da6468ac826c939ca40b714b1b292426
0xSJ <jainsanmati846@gmail.com>
2022-12-21 02:07:32 +0530

    Fix twitter meta tags

--- components/Container.tsx
+++ components/Container.tsx
@@ -81,7 +81,7 @@ export default function Container(props) {
   const meta = {
     title: 'Sahil Jain – Full Stack Web3 Developer',
     description: `Full Stack Web3 developer, Web3 enthusiast, and Content Creator.`,
-    image: 'https://sahiljain.io/static/avatar.jpg',
+    image: 'https://sahiljain.io/avatar.jpg',
     type: 'website',
     ...customMeta
   };
@@ -103,7 +103,7 @@ export default function Container(props) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@0xsahiljain" />
+        <meta name="twitter:site" content="@sahiljaindotio" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
